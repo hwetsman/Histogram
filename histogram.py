b204842133d13f35746cf695c1367b3d298617a2
hwetsman <howard@tocdr.com>
2022-02-03 08:01:01 -0600

    Code: create first inputs

--- histogram.py
+++ histogram.py
@@ -7,3 +7,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+
+st.write('Change the inputs in the sidebar to change the output of the plot')
+
+step = st.sidebar.slider('Step', min_value=1, max_value=10, value=5, step=1)
+mean = st.sidebar.slider('Mean of Distribution', min_value=100, max_value=200, value=100, step=5)
+sd = st.sidebar.slider('sd of Distribution', min_value=1, max_value=5, value=2, step=1)
