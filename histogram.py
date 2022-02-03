76b63e1588f10f6589aa43cda4ebddf285e72057
hwetsman <howard@tocdr.com>
2022-02-03 08:04:54 -0600

    Refactor: add N to input

--- histogram.py
+++ histogram.py
@@ -13,3 +13,6 @@ st.write('Change the inputs in the sidebar to change the output of the plot')
 step = st.sidebar.slider('Step', min_value=1, max_value=10, value=5, step=1)
 mean = st.sidebar.slider('Mean of Distribution', min_value=100, max_value=200, value=100, step=5)
 sd = st.sidebar.slider('sd of Distribution', min_value=1, max_value=5, value=2, step=1)
+size = st.sidebar.slider('N', min_value=1000, max_value=10000, value=5000, step=500)
+
+array = np.random.normal(mean, sd, size)
