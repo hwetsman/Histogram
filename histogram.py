6221c257930c3e12c279cea5004157becc6dba3f
hwetsman <howard@tocdr.com>
2022-02-03 07:54:49 -0600

    Code: imports and goal

---
+++ histogram.py
@@ -0,0 +1,9 @@
+"""To provide a variable input for users to develop parameters for a
+visualized histogram
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
